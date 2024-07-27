2dfff3c29413747b816af6d35e8c5bb90cef869b
4b-n4v <ab.navilon@gmail.com>
2024-07-27 17:55:15 -0500

    Added more display modes

--- monitor.sh
+++ monitor.sh
@@ -2,13 +2,11 @@
 # Variables
 monitor_file="/home/$USER/.config/hypr/monitors.conf"
 test_file="/home/$USER/Projects/shell-scripts/testfile.txt"
-banner=$"
-# +==================================+
+banner=$"# +==================================+
 # |░█▄░▄█░█▀█░█▀█░▀█▀░▀█▀░█▀█░█▀▄░█▀▀|
 # |░█░█░█░█░█░█░█░░█░░░█░░█░█░█▀▄░▀▀█|
 # |░▀░ ░▀░▀▀▀░▀░▀░▀▀▀░░▀░░▀▀▀░▀░▀░▀▀▀|
 # +==================================+
-
 "
 dock ()
 {
@@ -36,19 +34,27 @@ second144 ()
 	echo "monitor=HDMI-A-1,1920x1080@144,0x0,1.0" >> $test_file
 }
 
+secondtv()
+{
+	echo "$banner" > $test_file
+	echo "monitor=HDMI-A-1,1920x1080@60,0x0,1.0" >> $test_file
+}
+
 echo "Display Configuration"
 echo "Choose your options"
-echo "1.)   Dock"
-echo "2.) 󰍹 Extend144"
-echo "3.) 󰍹  Second144"
+echo "1.)    Dock"
+echo "2.) 󰍹  Extend144"
+echo "3.) 󰍹󰟴 Extend-TV"
+echo "4.) 󰍹   Second144"
+echo "5.) 󰍹󰟴  Second-TV"
 read -p "Choose: " choice
 
 
 case "$choice" in
 	1) dock;;
 	2) extend144;;
-	3) secondtv;;
-	*) echo "You didnt choose anything dumbass";;
+	3) extendtv;;
+	4) second144;;
+	5) secondtv;;
+	*) echo "Invalid Option!";;
 esac 
-	
-
